a4275646a78addea826c23d10d10ef69d1619d6f
5021201 <navid_email@hotmail.com>
2015-10-05 16:27:23 +1100

    tree

--- ass2/ass2/spec/Tree.java
+++ ass2/ass2/spec/Tree.java
@@ -1,5 +1,11 @@
 package ass2.spec;
 
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.glu.GLU;
+
+import com.jogamp.opengl.util.gl2.GLUT;
+
 /**
  * COMMENT: Comment Tree 
  *
@@ -20,5 +26,24 @@ public class Tree {
         return myPos;
     }
     
+    public void draw(GL2 gl) { 
+    	gl.glPushMatrix();
+    	
+    	GLU glu = new GLU();
+	    GLUT glut = new GLUT();
+		
+	    gl.glTranslated (myPos[0], myPos[1],  myPos[2]);    	    
+	    gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+
+        gl.glRotated(270, 1, 0, 0);  
+        glut.glutSolidCylinder(0.2,1.2,24,24); 
+        
+	    gl.glTranslated(0,0,1.2);  
+	    glut.glutSolidSphere(0.55, 24, 24);
+
+
+	    
+	    gl.glPopMatrix();
+}
 
 }
